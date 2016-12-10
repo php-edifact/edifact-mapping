62de6e9dc16b63ce109b402b494fbd4b04156b3e
sabas <sabas88@gmail.com>
2016-12-10 17:04:19 +0100

    Adding setters and methods to explore available directories and messages

--- src/Mapping/MappingProvider.php
+++ src/Mapping/MappingProvider.php
@@ -1,12 +1,14 @@
 <?php
 namespace EDI\Mapping;
 
-class MappingProvider {
+class MappingProvider
+{
 
     private $directory = 'D95B';
     private $path;
 
-    public function __construct($directory = 'D95B', $path = null) {
+    public function __construct($directory = 'D95B', $path = null)
+    {
         if (!defined("SEPARATOR")) {
             if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
                 define("SEPARATOR", "\\");
@@ -23,27 +25,63 @@ class MappingProvider {
         $this->path = $path;
     }
 
-    public function getCodes() {
+    public function setDirectory($directory = 'D95B')
+    {
+        $this->directory = $directory;
+    }
+
+    public function setPath($path = null)
+    {
+        $this->path = $path;
+    }
+
+    public function getCodes()
+    {
         return $this->path.SEPARATOR.$this->directory.SEPARATOR."codes.xml";
     }
 
-    public function getSegments() {
+    public function getSegments()
+    {
         return $this->path.SEPARATOR.$this->directory.SEPARATOR."segments.xml";
     }
 
-    public function getMessage($message = "codeco") {
+    public function getMessage($message = "codeco")
+    {
         return $this->path.SEPARATOR.$this->directory.SEPARATOR."messages".SEPARATOR.strtolower($message).".xml";
     }
 
-    public function getBasePath() {
+    public function getBasePath()
+    {
         return $this->path;
     }
-    
-    public function getServiceSegments($version = '3') {
+
+    public function getServiceSegments($version = '3')
+    {
         return $this->path.SEPARATOR."Service_V".$version.SEPARATOR."segments.xml";
     }
 
-    public function getServiceMessages($version = '3', $message = 'contrl') {
+    public function getServiceMessages($version = '3', $message = 'contrl')
+    {
         return $this->path.SEPARATOR."Service_V".$version.SEPARATOR."messages".SEPARATOR.strtolower($message).".xml";
+    }
+
+    /*
+     * Get message names from folder in this directory
+     */
+    public function listMessages()
+    {
+        $messages = array_slice(scandir($this->path.SEPARATOR.$this->directory.SEPARATOR."messages"), 2);
+        foreach ($messages as &$msg) {
+            $msg = str_replace('.xml', '', $msg);
+        }
+        return $messages;
+    }
+
+    /*
+     * Get directory names from this folder
+     */
+    public function listDirectories()
+    {
+        return array_diff(scandir($this->path), ['.', '..', 'MappingProvider.php']);
     }
 }
